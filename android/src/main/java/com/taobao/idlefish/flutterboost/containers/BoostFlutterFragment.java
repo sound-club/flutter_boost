f4f388d23745fc67b61f4c0af8ccd8998ac58caa
Eduardo Ribas <eduardo.ribas@sound.club>
2019-08-01 00:01:25 -0300

    Migrate to AndroidX

--- android/src/main/java/com/taobao/idlefish/flutterboost/containers/BoostFlutterFragment.java
+++ android/src/main/java/com/taobao/idlefish/flutterboost/containers/BoostFlutterFragment.java
@@ -23,10 +23,10 @@
  */
 package com.taobao.idlefish.flutterboost.containers;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -36,7 +36,6 @@ import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 
 import com.taobao.idlefish.flutterboost.BoostFlutterView;
-import com.taobao.idlefish.flutterboost.Debuger;
 import com.taobao.idlefish.flutterboost.FlutterBoostPlugin;
 import com.taobao.idlefish.flutterboost.interfaces.IFlutterViewContainer;
 
--- example/android/app/src/main/java/com/taobao/idlefish/flutterboostexample/FlutterFragmentPageActivity.java
+++ example/android/app/src/main/java/com/taobao/idlefish/flutterboostexample/FlutterFragmentPageActivity.java
@@ -2,11 +2,12 @@ package com.taobao.idlefish.flutterboostexample;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 import io.flutter.plugin.platform.PlatformPlugin;
 
@@ -34,7 +35,7 @@ public class FlutterFragmentPageActivity extends AppCompatActivity {
 
         mFragment = FlutterFragment.instance("hello");
 
-        getSupportFragmentManager()
+        getFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragment_stub,mFragment)
                 .commit();
